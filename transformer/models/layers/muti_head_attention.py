482882383a73c45ee59c9b81a13dc08f1e6717af
承念 <1791708451@qq.com>
2023-08-20 19:09:30 +0800

    add dot product and attention

--- transformer/models/layers/muti_head_attention.py
+++ transformer/models/layers/muti_head_attention.py
@@ -1,7 +1,57 @@
 '''
 Date: 2023-08-20 00:16:26
 LastEditors: turtlepig
-LastEditTime: 2023-08-20 00:16:29
+LastEditTime: 2023-08-20 19:08:28
 Description:  Multi-Head Attention
 '''
 import torch.nn as nn
+
+from models.layers.scale_dot_product_attention import ScaleDotProductAttention
+
+class MulitHeadAttention(nn.Module):
+    
+    def __init__(self, d_model, n_head):
+        super(MulitHeadAttention, self).__init__()
+        self.n_head = n_head
+        self.attention = ScaleDotProductAttention()
+        # 本质是一组映射权重
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_concat = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask = None):
+
+        # 1. dot product with weight matrices
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+
+        # 2. split tensor by number of heads
+
+
+    def split(self, tensor):
+        r"""
+        split tensor by number of head
+
+        :param tensor: [batch_size, length, d_model]
+        :return: [batch_size, head, length, d_tensor] | d_tensor = d_model / n_head
+        """
+        batch_size, length, d_model = tensor.size()
+
+        d_tensor = d_model // self.n_head
+        tensor = tensor.view(batch_size, length, self.n_head, d_tensor).transpose(1, 2)
+
+        return tensor
+    
+    def concat(self, tensor):
+        r"""
+        inverse function of self.split(tensor : torch.Tensor)
+
+        :param tensor: [batch_size, head, length, d_tensor]
+        :return: [batch_size, length, d_model]
+        """
+        batch_size, head, length, d_tensor = tensor.size()
+        d_model = head * d_tensor
+
+        tensor = tensor.transpose(1,2).contiguous().view(batch_size, length, d_model)
+        
+        return tensor
--- transformer/models/layers/scale_dot_product_attention.py
+++ transformer/models/layers/scale_dot_product_attention.py
@@ -1,6 +1,43 @@
 '''
 Date: 2023-08-20 00:24:06
 LastEditors: turtlepig
-LastEditTime: 2023-08-20 00:24:08
+LastEditTime: 2023-08-20 18:11:28
 Description:  Scale Dot Product Attention
 '''
+import math
+
+import torch.nn as nn
+
+class ScaleDotProductAttention(nn.Module):
+    r"""
+    compute scale dot product attention
+
+    Query : given sentence that we focused on (decoder)
+    Key : every sentence to check relationship with Qeury(encoder)
+    Value : every sentence same with Key (encoder)
+    """
+    def __init__(self):
+        super(ScaleDotProductAttention, self).__init__()
+        self.softmax = nn.Softmax(dim = 1)
+    
+    def forward(self, q, k, v , mask =None, e = 1e-12):
+        # input is 4 dimension tensor
+        # [batch_size, head, length, d_tensor]
+
+        bath_size, head, length, d_tensor = k.size()
+
+        # 1. dot product Query with Key^T to compute similarity
+        k_t = k.transpose(2, 3) # 下标从0开始
+        score = (q @ k_t) / math.sqrt(d_tensor) # scaled dot product
+
+        # 2. apply masking (opt)
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -10000)
+
+        # 3. pass them softmax to make [0, 1] range
+        score = self.softmax(score)
+
+        # 4. multiply with Value
+        v = score @ v
+
+        return v, score
